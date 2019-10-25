068266e3987a477bc182df1240758d68043f0503
timurrrr <timurrrr@google.com>
2019-10-25 13:19:41 -0700

    Roll forward "Try using ParcelFileDescriptor for local files"
    PiperOrigin-RevId: 276747771

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/DefaultImageHeaderParser.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/DefaultImageHeaderParser.java
@@ -86,19 +86,18 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
   @NonNull
   private ImageType getType(Reader reader) throws IOException {
     final int firstTwoBytes = reader.getUInt16();
-
     // JPEG.
     if (firstTwoBytes == EXIF_MAGIC_NUMBER) {
       return JPEG;
     }
 
-    final int firstFourBytes = (firstTwoBytes << 16 & 0xFFFF0000) | (reader.getUInt16() & 0xFFFF);
+    final int firstFourBytes = (firstTwoBytes << 16) | reader.getUInt16();
     // PNG.
     if (firstFourBytes == PNG_HEADER) {
       // See: http://stackoverflow.com/questions/2057923/how-to-check-a-png-for-grayscale-alpha
       // -color-type
       reader.skip(25 - 4);
-      int alpha = reader.getByte();
+      int alpha = reader.getUInt8();
       // A RGB indexed PNG can also have transparency. Better safe than sorry!
       return alpha >= 3 ? PNG_A : PNG;
     }
@@ -115,26 +114,26 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
     }
     // Bytes 4 - 7 contain length information. Skip these.
     reader.skip(4);
-    final int thirdFourBytes =
-        (reader.getUInt16() << 16 & 0xFFFF0000) | (reader.getUInt16() & 0xFFFF);
+    final int thirdFourBytes = (reader.getUInt16() << 16) | reader.getUInt16();
     if (thirdFourBytes != WEBP_HEADER) {
       return UNKNOWN;
     }
-    final int fourthFourBytes =
-        (reader.getUInt16() << 16 & 0xFFFF0000) | (reader.getUInt16() & 0xFFFF);
+    final int fourthFourBytes = (reader.getUInt16() << 16) | reader.getUInt16();
     if ((fourthFourBytes & VP8_HEADER_MASK) != VP8_HEADER) {
       return UNKNOWN;
     }
     if ((fourthFourBytes & VP8_HEADER_TYPE_MASK) == VP8_HEADER_TYPE_EXTENDED) {
       // Skip some more length bytes and check for transparency/alpha flag.
       reader.skip(4);
-      return (reader.getByte() & WEBP_EXTENDED_ALPHA_FLAG) != 0 ? ImageType.WEBP_A : ImageType.WEBP;
+      short flags = reader.getUInt8();
+      return (flags & WEBP_EXTENDED_ALPHA_FLAG) != 0 ? ImageType.WEBP_A : ImageType.WEBP;
     }
     if ((fourthFourBytes & VP8_HEADER_TYPE_MASK) == VP8_HEADER_TYPE_LOSSLESS) {
       // See chromium.googlesource.com/webm/libwebp/+/master/doc/webp-lossless-bitstream-spec.txt
       // for more info.
       reader.skip(4);
-      return (reader.getByte() & WEBP_LOSSLESS_ALPHA_FLAG) != 0 ? ImageType.WEBP_A : ImageType.WEBP;
+      short flags = reader.getUInt8();
+      return (flags & WEBP_LOSSLESS_ALPHA_FLAG) != 0 ? ImageType.WEBP_A : ImageType.WEBP;
     }
     return ImageType.WEBP;
   }
@@ -237,11 +236,12 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
         return -1;
       }
 
-      // Segment length includes bytes for segment length.
-      int segmentLength = reader.getUInt16() - 2;
+      int segmentLength = reader.getUInt16();
+      // A segment includes the bytes that specify its length.
+      int segmentContentsLength = segmentLength - 2;
       if (segmentType != EXIF_SEGMENT_TYPE) {
-        long skipped = reader.skip(segmentLength);
-        if (skipped != segmentLength) {
+        long skipped = reader.skip(segmentContentsLength);
+        if (skipped != segmentContentsLength) {
           if (Log.isLoggable(TAG, Log.DEBUG)) {
             Log.d(
                 TAG,
@@ -249,14 +249,14 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
                     + ", type: "
                     + segmentType
                     + ", wanted to skip: "
-                    + segmentLength
+                    + segmentContentsLength
                     + ", but actually skipped: "
                     + skipped);
           }
           return -1;
         }
       } else {
-        return segmentLength;
+        return segmentContentsLength;
       }
     }
   }
@@ -393,15 +393,35 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
   }
 
   private interface Reader {
+
+    /**
+     * Reads and returns a 8-bit unsigned integer.
+     *
+     * <p>Throws an {@link EndOfFileException} if an EOF is reached.
+     */
+    short getUInt8() throws IOException;
+
+    /**
+     * Reads and returns a 16-bit unsigned integer.
+     *
+     * <p>Throws an {@link EndOfFileException} if an EOF is reached.
+     */
     int getUInt16() throws IOException;
 
-    short getUInt8() throws IOException;
+    /**
+     * Reads and returns a byte array.
+     *
+     * <p>Throws an {@link EndOfFileException} if an EOF is reached before anything was read.
+     */
+    int read(byte[] buffer, int byteCount) throws IOException;
 
     long skip(long total) throws IOException;
 
-    int read(byte[] buffer, int byteCount) throws IOException;
-
-    int getByte() throws IOException;
+    final class EndOfFileException extends IOException {
+      EndOfFileException() {
+        super("Unexpectedly reached end of a file");
+      }
+    }
   }
 
   private static final class ByteBufferReader implements Reader {
@@ -414,20 +434,16 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
     }
 
     @Override
-    public int getUInt16() {
-      return (getByte() << 8 & 0xFF00) | (getByte() & 0xFF);
+    public short getUInt8() throws EndOfFileException {
+      if (byteBuffer.remaining() < 1) {
+        throw new EndOfFileException();
+      }
+      return (short) (byteBuffer.get() & 0xFF);
     }
 
     @Override
-    public short getUInt8() {
-      return (short) (getByte() & 0xFF);
-    }
-
-    @Override
-    public long skip(long total) {
-      int toSkip = (int) Math.min(byteBuffer.remaining(), total);
-      byteBuffer.position(byteBuffer.position() + toSkip);
-      return toSkip;
+    public int getUInt16() throws EndOfFileException {
+      return ((int) getUInt8() << 8) | getUInt8();
     }
 
     @Override
@@ -441,11 +457,10 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
     }
 
     @Override
-    public int getByte() {
-      if (byteBuffer.remaining() < 1) {
-        return -1;
-      }
-      return byteBuffer.get();
+    public long skip(long total) {
+      int toSkip = (int) Math.min(byteBuffer.remaining(), total);
+      byteBuffer.position(byteBuffer.position() + toSkip);
+      return toSkip;
     }
   }
 
@@ -458,13 +473,34 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
     }
 
     @Override
-    public int getUInt16() throws IOException {
-      return (is.read() << 8 & 0xFF00) | (is.read() & 0xFF);
+    public short getUInt8() throws IOException {
+      int readResult = is.read();
+      if (readResult == -1) {
+        throw new EndOfFileException();
+      }
+
+      return (short) readResult;
     }
 
     @Override
-    public short getUInt8() throws IOException {
-      return (short) (is.read() & 0xFF);
+    public int getUInt16() throws IOException {
+      return ((int) getUInt8() << 8) | getUInt8();
+    }
+
+    @Override
+    public int read(byte[] buffer, int byteCount) throws IOException {
+      int numBytesRead = 0;
+      int lastReadResult = 0;
+      while (numBytesRead < byteCount
+          && ((lastReadResult = is.read(buffer, numBytesRead, byteCount - numBytesRead)) != -1)) {
+        numBytesRead += lastReadResult;
+      }
+
+      if (numBytesRead == 0 && lastReadResult == -1) {
+        throw new EndOfFileException();
+      }
+
+      return numBytesRead;
     }
 
     @Override
@@ -492,21 +528,6 @@ public final class DefaultImageHeaderParser implements ImageHeaderParser {
         }
       }
       return total - toSkip;
-    }
-
-    @Override
-    public int read(byte[] buffer, int byteCount) throws IOException {
-      int toRead = byteCount;
-      int read;
-      while (toRead > 0 && ((read = is.read(buffer, byteCount - toRead, toRead)) != -1)) {
-        toRead -= read;
-      }
-      return byteCount - toRead;
-    }
-
-    @Override
-    public int getByte() throws IOException {
-      return is.read();
     }
   }
 }
